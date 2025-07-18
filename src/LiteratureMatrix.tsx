bf28f2735f922bbb13c70dc4bf22ab3653963b77
leafboo <janjancaranguian@gmail.com>
2025-07-18 14:07:20 +0800

    Finish the styling

--- src/LiteratureMatrix.tsx
+++ src/LiteratureMatrix.tsx
@@ -1,9 +1,42 @@
 import LiteratureMatrixRow from "./LiteratureMatrixRow"
+import dummyData from "../dummy-data.json"
 
 export default function LiteratureMatrix() {
+
+    const LitMatrixRowElement = dummyData.data.map(element => <LiteratureMatrixRow title={element.title}
+                                                                                    authors={element.authors}
+                                                                                    publicationYear={element.publicationYear}
+                                                                                    abstract={element.abstract}
+                                                                                    methods={element.methods}
+                                                                                    findings={element.findings} /> )
+
+    
     return (
         <>
-            <div> literature matrix</div>
+            <br />
+            <div className="flex justify-center text-[30px]"> Literature Matrix</div><br />
+            <table className=" w-[80rem] ml-auto mr-auto text-left">
+                <thead>
+                    <tr>
+                        <th className="p-[1rem] border-[1px]">Title</th>
+                        <th className="p-[1rem] border-[1px]">Authors</th>
+                        <th className="p-[1rem] border-[1px]">Publication Year</th>
+                        <th className="p-[1rem] border-[1px]">Abstract</th>
+                        <th className="p-[1rem] border-[1px]">Methods</th>
+                        <th className="p-[1rem] border-[1px]">Findings</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {LitMatrixRowElement}
+                </tbody>
+                
+                
+                
+            </table>
+            <br /><br />
+            <div className="flex justify-center">
+                <button className="p-[10px] border-[1px] cursor-pointer">Download as PDF</button>
+            </div>
         </>
     )
 }
--- src/LiteratureMatrixRow.tsx
+++ src/LiteratureMatrixRow.tsx
@@ -1,6 +1,24 @@
-export default function LiteratureMatrixRow() {
+type LiteratureMatrixRowProps ={
+    title: string;
+    authors: string;
+    publicationYear: number;
+    abstract: string;
+    methods: string;
+    findings: string
+}
+
+export default function LiteratureMatrixRow(props: LiteratureMatrixRowProps) {
+
     return (
         <>
+            <tr className="">
+                <td className="p-[1rem] border-[1px]">{props.title}</td>
+                <td className="p-[1rem] border-[1px]">{props.authors}</td>
+                <td className="p-[1rem] border-[1px]">{props.publicationYear}</td>
+                <td className="p-[1rem] border-[1px]">{props.abstract}</td>
+                <td className="p-[1rem] border-[1px]">{props.methods}</td>
+                <td className="p-[1rem] border-[1px]">{props.findings}</td>
+            </tr>
         </>
     )
 }
